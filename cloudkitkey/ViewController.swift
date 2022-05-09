97c711bd99e8b3ec78e9781c2573a63a87710d08
nkalupahana <avishkank@gmail.com>
2022-05-09 10:37:51 -0700

    Error reproduced

--- cloudkitkey/ViewController.swift
+++ cloudkitkey/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
 
@@ -15,5 +16,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func click(_ sender: Any) {
+        let operation = CKFetchWebAuthTokenOperation(apiToken: "d43e4a0f0eac5ab776190238b97c415e847d045760d3608d75994379dd02a565")
+        operation.fetchWebAuthTokenResultBlock = { res in
+            // CKError Service Unavailable on private cloud database
+            print(res)
+        }
+        
+        // This gets a key, but the key doesn't work with private db operations
+        //  (ACCESS_DENIED, private db access disabled for this account)
+        //CKContainer(identifier: "iCloud.baseline.getbaseline.app").publicCloudDatabase.add(operation);
+        
+        CKContainer(identifier: "iCloud.baseline.getbaseline.app").privateCloudDatabase.add(operation);
+    }
 }
 
