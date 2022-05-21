6bb9e20ae95732077edfc13f3a00a712444eccba
nkalupahana <avishkank@gmail.com>
2022-05-21 07:26:05 -0700

    Added URL encoding to token output

--- cloudkitkey/ViewController.swift
+++ cloudkitkey/ViewController.swift
@@ -17,15 +17,16 @@ class ViewController: UIViewController {
 
 
     @IBAction func click(_ sender: Any) {
+        print("Getting...")
         let operation = CKFetchWebAuthTokenOperation(apiToken: "d43e4a0f0eac5ab776190238b97c415e847d045760d3608d75994379dd02a565")
         operation.fetchWebAuthTokenResultBlock = { res in
-            // CKError Service Unavailable on private cloud database
-            print(res)
+            do {
+                let result = try res.get()
+                print(result.addingPercentEncoding(withAllowedCharacters: NSCharacterSet(charactersIn: ";,/?:@&=+$# ").inverted)!)
+            } catch {
+                print(error)
+            }
         }
-        
-        // This gets a key, but the key doesn't work with private db operations
-        //  (ACCESS_DENIED, private db access disabled for this account)
-        //CKContainer(identifier: "iCloud.baseline.getbaseline.app").publicCloudDatabase.add(operation);
         
         CKContainer(identifier: "iCloud.baseline.getbaseline.app").privateCloudDatabase.add(operation);
     }
